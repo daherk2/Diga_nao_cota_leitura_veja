25840047b8a4e8164c54a0b40cd36bc8d6156bb8
Fabio Rosindo Daher de Barros <fabio.rosindo.daher.de.barros@gmail.com>
2018-08-18 03:45:48 -0300

    Initial Commit
    Initial commit

---
+++ loader.js
@@ -0,0 +1,6 @@
+window.onload = function () { 
+	document.getElementById("piano_offer").style.visibility = "hidden";
+	document.body.style.overflow = "auto";
+	document.getElementsByClassName("piano-offer-overlay")[0].classList.remove("piano-offer-overlay");
+}
+
